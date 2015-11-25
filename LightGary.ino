f14886e779779936885997068e0c22e5df802e2b
m.papa <m.papa@7e789f6c-1a6a-4a42-96ee-5e3e0c5be405>
2015-11-25 01:29:31 +0000

    Uhuu. At last a good signal from the encoder. Now trying to get bouncing away.

--- LightGary.ino
+++ LightGary.ino
@@ -1,42 +1,34 @@
-const int PinCLK = 2; // Used for generating interrupts using CLK signal
-const int PinDT = 3; // Used for reading DT signal
-const int PinSW = 8; // Used for the push button switch
+#include <Encoder.h>
 
-static long virtualPosition = 0; // without STATIC it does not count correctly!!!
+const int MetalCLK = 2; // Used for generating interrupts using CLK signal
+const int MetalDT = 4; // Used for reading DT signal
+const int MetalSwitch = 8; // Used for the push button switch
 
-void isr0 () {
-  detachInterrupt(0);
-  if (!digitalRead(PinDT))
-    virtualPosition++;
-  else
-    virtualPosition--;
-  attachInterrupt (0, isr0, RISING);
-}
+long metalPosition = 0;
+
+Encoder metalEncoder(MetalCLK, MetalDT);
+
 
 void setup () {
-  pinMode(PinCLK, INPUT);
-  pinMode(PinDT, INPUT);
-  pinMode(PinSW, INPUT);
-  attachInterrupt (0, isr0, RISING); // interrupt 0 is always connected to pin 2 on Arduino UNO
+  pinMode(MetalSwitch, INPUT);
   Serial.begin (9600);
-  Serial.println("Start");
+  Serial.println("START");
 }
 
 void loop () {
-  int lastCount = 0;
+
+    long metalRead = metalEncoder.read();
+    
+    if (digitalRead(MetalSwitch)) {  // check if pushbutton is pressed
+      metalEncoder.write(0);    
+      while (digitalRead(MetalSwitch)) {} 
+      Serial.println("Reset to zero");
+    }
+    
+    if (metalPosition != metalRead) {
+      metalPosition = metalRead;
+      Serial.print("Count: ");
+      Serial.println(metalPosition);
+    }
   
-  while (true) {
-    if (!(digitalRead(PinSW))) {  // check if pushbutton is pressed
-      virtualPosition = 0;        // if YES, then reset counter to ZERO
-      while (!digitalRead(PinSW)) {
-        // wait til switch is released
-      } 
-      Serial.println("Reset");
-    }
-    if (virtualPosition != lastCount) {
-      lastCount = virtualPosition;
-      Serial.print("Count:");
-      Serial.println(virtualPosition);
-    }
-  }
 }
